0f3b13c315222586edb626dd08fb73e6e05d7c84
shrutiso05 <shristisoni051199@gmail.com>
2024-01-20 19:52:49 +0530

    set_up_react_next_js

---
+++ pages/api/auth/nextauth.js
@@ -0,0 +1,15 @@
+// pages/api/auth/[...nextauth].js
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+
+export default NextAuth({
+  providers: [
+    Providers.Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  pages: {
+    signIn: '/auth/signin',
+  },
+});
